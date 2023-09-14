bc79834fd8fc3a76a5d61af0963efcd90a134ec2
umutbariscoskun <umutcoskun2017@gmail.com>
2023-09-14 22:11:24 +0300

    update main.dart with router

--- lib/core/config/dependency_injection/injectable.config.dart
+++ lib/core/config/dependency_injection/injectable.config.dart
@@ -10,7 +10,8 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:api_crud_app/core/config/dependency_injection/module.dart'
-    as _i8;
+    as _i9;
+import 'package:api_crud_app/core/config/router/router.dart' as _i7;
 import 'package:api_crud_app/features/crud_app/data/datasource/remote/account_remote_datasource.dart'
     as _i3;
 import 'package:api_crud_app/features/crud_app/data/repository/account_repository_impl.dart'
@@ -20,7 +21,7 @@ import 'package:api_crud_app/features/crud_app/domain/repository/account_reposit
 import 'package:api_crud_app/features/crud_app/domain/usecase/account_usecases/account_usecases.dart'
     as _i6;
 import 'package:api_crud_app/features/crud_app/presentation/home/cubit/home_cubit.dart'
-    as _i7;
+    as _i8;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -42,6 +43,7 @@ extension GetItInjectableX on _i1.GetIt {
         () => _i5.AccountRepositoryImpl(gh<_i3.AccountRemoteDataSource>()));
     gh.factory<_i6.AddAccountUseCase>(
         () => _i6.AddAccountUseCase(gh<_i4.AccountRepository>()));
+    gh.lazySingleton<_i7.AppRouter>(() => _i7.AppRouter());
     gh.factory<_i6.GetAccountsUseCase>(
         () => _i6.GetAccountsUseCase(gh<_i4.AccountRepository>()));
     gh.factory<_i6.RemoveAccountUseCase>(
@@ -54,10 +56,10 @@ extension GetItInjectableX on _i1.GetIt {
           removeAccountUseCase: gh<_i6.RemoveAccountUseCase>(),
           updateAccountUseCase: gh<_i6.UpdateAccountUseCase>(),
         ));
-    gh.factory<_i7.HomeCubit>(
-        () => _i7.HomeCubit(accountUseCases: gh<_i6.AccountUseCases>()));
+    gh.factory<_i8.HomeCubit>(
+        () => _i8.HomeCubit(accountUseCases: gh<_i6.AccountUseCases>()));
     return this;
   }
 }
 
-class _$RegisterModule extends _i8.RegisterModule {}
+class _$RegisterModule extends _i9.RegisterModule {}
