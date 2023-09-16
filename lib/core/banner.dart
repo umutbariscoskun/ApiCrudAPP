b33d7731a687b96e4575b9606e8271e0411a1863
umutbariscoskun <umutcoskun2017@gmail.com>
2023-09-16 17:33:55 +0300

    add ios environment settings

---
+++ lib/core/banner.dart
@@ -0,0 +1,28 @@
+import 'package:api_crud_app/core/env.dart';
+import 'package:flutter/material.dart';
+
+class FlavorBanner extends StatelessWidget {
+  final Widget child;
+  final String message;
+
+  const FlavorBanner({
+    required this.child,
+    required this.message,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: EnvironmentConfig.isProd
+          ? child
+          : Banner(
+              message: message,
+              location: BannerLocation.topStart,
+              color: EnvironmentConfig.isStage ? Colors.red : Colors.green,
+              child: child,
+            ),
+    );
+  }
+}
--- makefile
+++ makefile
@@ -1,2 +1,14 @@
 build_runner:
 	flutter pub run build_runner build --delete-conflicting-outputs
+
+
+	#Builds the application for the dev environment.
+
+run-ios-debug-dev:
+	flutter run --target lib/main.dart --flavor test --dart-define=APP_ENV=test
+
+run-ios-debug-uat:
+	flutter run --target lib/main.dart --flavor uat --dart-define=APP_ENV=uat
+
+run-ios-debug-prod:
+	flutter run --target lib/main.dart --flavor prod --dart-define=APP_ENV=prod
